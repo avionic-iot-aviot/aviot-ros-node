1e3683e1d290a44fbc9690d0709fd3424020456e
aviot <asantangelo90@gmail.com>
2021-09-13 15:55:27 +0200

    Some small changes.

--- src/listener.js
+++ src/listener.js
@@ -6,7 +6,8 @@ function sleep(ms) {
 
 (async () => {
   // init ros
-  const rosNode = await rosnodejs.initNode(`/talker2`, {})
+  const rosNode = await rosnodejs.initNode(`/protoTestSend`, {})
+  //const rosNode = await rosnodejs.initNode(`/talker2`, {})
   
   // const sub = rosNode.subscribe('/chatter', 'std_msgs/String', (msg) => {
   //   console.log('Got msg on chatter: %j', msg);
@@ -18,9 +19,10 @@ function sleep(ms) {
   const pub1 = rosNode.advertise('/test/tcpTopic', 'std_msgs/String');
   const pub2 = rosNode.advertise('/test/udpTopic', 'std_msgs/String');
 
-  for(let i = 0;i < 100; i++){
-    pub1.publish({ data: "hi " + i });
-    pub2.publish({ data: "hi " + i });
+  for(let i = 0;i < 10000; i++){
+    console.log("publish "+i)
+    pub1.publish({ data: "hi tcp " + i });
+    pub2.publish({ data: "hi udp " + i });
     await sleep(2000)
   }
 })()
